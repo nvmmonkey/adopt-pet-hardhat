f3b219d4500f7984e0bf152e43dfb2053d36158d
Kit <nvmmonkey@github.com>
2023-11-03 16:02:09 -0700

    Function: adoptPet and mapping array for all adopted pets/petIndex for adopted address/Owner address ot adopted pet array[]

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.19;
 contract PetAdoption {
     address public owner;
     uint public petIndex = 0;
+    uint[] public allAdoptedPets;
+
+    mapping(uint => address) public petIdxToOwnerAddress;
+    mapping(address => uint[]) public ownerAddressToPetList;
 
     constructor(uint initialPetIndex) {
         owner = msg.sender;
@@ -18,6 +22,18 @@ contract PetAdoption {
         );
         petIndex++;
     }
+
+    function adoptPet(uint adoptIdx) public {
+        require(adoptIdx < petIndex, "Pet index out of bound!");
+        require(
+            petIdxToOwnerAddress[adoptIdx] == address(0),
+            "Pet is already adopted!"
+        );
+
+        petIdxToOwnerAddress[adoptIdx] = msg.sender; //map pet_3 => 0xabc..abc
+        ownerAddressToPetList[msg.sender].push(adoptIdx); //map 0xabc..abc => pet_3
+        allAdoptedPets.push(adoptIdx); //push adoptPetIndex to allAdoptedPets Array =>[3]
+    } 
 
     function getOwner() public view returns (address) {
         return owner;
