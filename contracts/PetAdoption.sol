fd816a70c1eaad7d0b3cf030561329f6d5aeac5d
Kit <nvmmonkey@github.com>
2023-11-03 16:59:47 -0700

    Test: test get func & arrays

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.19;
-import "hardhat/console.sol";
+
+// import "hardhat/console.sol";
 
 contract PetAdoption {
     address public owner;
@@ -31,22 +32,25 @@ contract PetAdoption {
             "Pet is already adopted!"
         );
 
-        console.log("Adopting pet: ", adoptIdx);
+        // console.log("Adopting pet: ", adoptIdx);
 
         petIdxToOwnerAddress[adoptIdx] = msg.sender; //map pet_3 => 0xabc..abc
 
-        console.log("New owner: ", petIdxToOwnerAddress[adoptIdx]);
-
+        // console.log("New owner: ", petIdxToOwnerAddress[adoptIdx]);
 
         ownerAddressToPetList[msg.sender].push(adoptIdx); //map 0xabc..abc => pet_3
         allAdoptedPets.push(adoptIdx); //push adoptPetIndex to allAdoptedPets Array =>[3]
-
-
-    } 
+    }
 
     function getOwner() public view returns (address) {
         return owner;
     }
 
-    
+    function getAllAdoptedPetsByOwner() public view returns (uint[] memory) {
+        return ownerAddressToPetList[msg.sender];
+    }
+
+    function getAllAdoptedPets() public view returns (uint[] memory) {
+        return allAdoptedPets;
+    }
 }
