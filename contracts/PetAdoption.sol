0091ef9138c3087eb39d5a7dbc772393c3db19b5
Kit <nvmmonkey@github.com>
2023-11-03 15:43:00 -0700

    Add addPet() index for pet minting index + test func

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -4,9 +4,18 @@ pragma solidity ^0.8.19;
 
 contract PetAdoption {
     address public owner;
+    uint public petIndex = 0;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function addPet() public {
+        require(
+            msg.sender == owner,
+            "Only a contract owner can add a new pet!"
+        );
+        petIndex++;
     }
 
     function getOwner() public view returns (address) {
--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -3,21 +3,23 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("PetAdoption", function () {
   async function deployContractFixture() {
-    const [owner] = await ethers.getSigners();
+    const [owner, account2] = await ethers.getSigners();
     const PetAdoption = await ethers.getContractFactory("PetAdoption");
     const contract = await PetAdoption.deploy();
 
     const randomNum = Math.random();
 
-    return { owner, contract, randomNum};
+    return { owner, account2, contract, randomNum };
   }
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
-      const { owner, contract, ...rest } = await loadFixture(deployContractFixture);
+      const { owner, contract, ...rest } = await loadFixture(
+        deployContractFixture
+      );
       const contractOwner = await contract.owner();
 
-    //   console.log("R1: " + randomNum);
+      //   console.log("R1: " + randomNum);
       console.log("Contract Owner: " + contractOwner);
       console.log("Deployer: " + owner.address);
 
@@ -25,14 +27,29 @@ describe("PetAdoption", function () {
     });
 
     it("getOwner() should return the right owner", async function () {
-      const { owner, contract, ...rest } = await loadFixture(deployContractFixture);
+      const { owner, contract, ...rest } = await loadFixture(
+        deployContractFixture
+      );
       const contractOwner = await contract.getOwner();
 
-    //   console.log("R2: " + randomNum);
+      //   console.log("R2: " + randomNum);
 
       expect(await contract.owner()).to.equal(owner.address);
     });
   });
+
+  describe("Add Pet", function () {
+    it("Should revert with the right error in case of other account", async function () {
+      const { owner, contract, account2 } = await loadFixture(
+        deployContractFixture
+      );
+
+      await expect(contract.connect(account2).addPet()).to.be.revertedWith(
+        "Only a contract owner can add a new pet!"
+      );
+    });
+  });
 });
 
-//npx hardhat test --network hardhat ==> test the contract
+//npx hardhat test --network hardhat ==> test the contract on hardhat
+//npx hardhat test --network localhost ==> test the contract on localhost
