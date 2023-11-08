06b973e349046e2d0e03959890a7101b72da1bae
Kit <nvmmonkey@github.com>
2023-11-08 00:34:22 -0800

    Deploy on Polygon Mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,34 @@
-require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+const keys = require("./keys.json");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-  networks:{
-    localhost:{}
-  }
+  networks: {
+    localhost: {},
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [keys.DEPLOY_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: keys.POLYGON_API_KEY,
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true,
+  },
+  solidity: {
+    version: "0.8.19",
+
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
